3cd861e3b7d800b94a32203e73cb82aaee680e23
田小瑞 <97039220+sytruix@users.noreply.github.com>
2025-04-28 02:59:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ class MyPlugin(Star):
     async def initialize(self):
         """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
     
-    # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("helloworld")
+    # 注册指令的装饰器。指令名为 Custom-reply。注册成功后，发送 `/Custom-reply` 就会触发这个指令，并回复 `你好, {user_name}!`
+    @filter.command("Custom-reply")
     async def helloworld(self, event: AstrMessageEvent):
-        """这是一个 hello world 指令""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
+        """这是一个 Custom-reply 指令""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
         user_name = event.get_sender_name()
         message_str = event.message_str # 用户发的纯文本消息字符串
         message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
